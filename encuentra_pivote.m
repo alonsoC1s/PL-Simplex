043fc40e523693269c0e5b305df202581e1e9541
Alonso Martinez <alonso.mtz99@gmail.com>
2020-10-28 00:20:13 -0600

    Mejora detección de pivote

--- encuentra_pivote.m
+++ encuentra_pivote.m
@@ -2,24 +2,27 @@ function [i,j] = encuentra_pivote(A)
     % Iterando sobre costos de izq a derecha as per Bland
     % Se asume que ya hay una identidad. Si no la hay se debió manejar antes
     [m, n] = size(A);
-    b = A(:, n);
+    b = A(1:m-1, n);
     costos = A(m, 1:n-1);
 
     % Identificamos la columna pivote (j)
+    hayPivote = false;
     for j = 1:n-1
+	fprintf("Probando con %i\n", j);
         if costos(j) < 0 
+	    hayPivote = true;
             break
         end
     end
 
     % Checamos si hubo costo negativo
-    if j == n-1
+    if hayPivote  == false
         % Recorrio todo y no hay. No hay pivote
         i = 0; j = 0; % Indice 0 (no permitido) afuera se entiende como: no hay pivote
         return 
     end
 
     % Identificamos una columna pivote. Ahora vemos que entrada de la columna es pivote
-    epsilons = b ./ (A(:,j) .* (A(:,j ) > 0)); % Dividiendo b entre los coefs de col. A no-negativos 
+    epsilons = b ./ (A(1:m-1 ,j) .* (A(1:m-1 ,j ) > 0)) % Dividiendo b entre los coefs de col. A no-negativos 
     [epi, i] = min(epsilons(not(isinf(epsilons))));
 end 
