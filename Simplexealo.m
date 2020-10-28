241db327176d52a54d55796a884f85ab57acb548
Rebeca98 <rbbkitanias@gmail.com>
2020-10-28 14:50:43 -0600

    añadimos al codigo la condicion para que nos diga cuando no hay solucion

--- Simplexealo.m
+++ Simplexealo.m
@@ -1,10 +1,20 @@
 function A = Simplexealo(A)
 
 [i,j] = encuentra_pivote(A);
-    while  i, j ~= 0 
-        A =  pivotea(A, i,j);
-        [i,j] = encuentra_pivote(A);
-        
+ 
+    while  i, j ~= 0;
+        disp('primero while');
+        A =  pivotea(A, i,j)
+        [i,j] = encuentra_pivote(A);     
     end
+
+tf = isempty(i);
+        if tf == 1
+            disp('no tiene solución');
+        end    
+epi = encuentra_pivote(A);
+    if epi == 0
+            disp('es una solución degenerada');
+    end  
     
 end
