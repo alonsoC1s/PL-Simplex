10d8f1ff6de95fce28ed9eb7af37e5f2e60668d6
Alonso Martinez <alonso.mtz99@gmail.com>
2020-10-28 20:39:48 -0600

    Añadido ejemplo que muestra que el ciclado es por error numerico

--- pivotea.m
+++ pivotea.m
@@ -1,7 +1,9 @@
 function B = pivotea(A, i, j)
 	[m, n] = size(A);
+	A = sym(A);
 
 	% Dividiendo el renglon que contiene el pivote para obtener un 1 principal
+	% Suena a que se puede hacer toda la operacion en una sola matriz elemental pero no es asi
 	A = diag([ones(1,i-1), A(i,j)^-1, ones(1,m-i)]) * A;
 
 	% Obteniendo los factores t.q R_i - efes_i * pivote = 0
